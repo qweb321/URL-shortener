85ed3fe4ea6279dccf499964b60283616724cfcc
Isis Lin <qweb32114@gmail.com>
2022-11-17 14:14:32 +0800

    feat: add style.css and sweetaler2 module for alert box

--- app.js
+++ app.js
@@ -11,20 +11,22 @@ const app = express();
 app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: "hbs" }));
 app.set("view engine", "hbs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/images", express.static("images"));
+app.use("/public", express.static("public"));
+app.use(express.static("node_modules/sweetalert2"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const image = { src: "images/link-icon.png" };
+  res.render("index", { image });
 });
 
 app.post("/", (req, res) => {
+  const image = { src: "images/link-icon.png" };
   let shortenId = shortenID();
   //check shortenID exist or not
   Url.findOne({ urlID: shortenId }).then((id) => {
     if (id) {
-      console.log("exist");
-      console.log(shortenId);
-    } else {
-      console.log("not exist");
+      // if shortenId alreddy exist in database, create new one
       shortenId = shortenID();
     }
   });
@@ -34,11 +36,10 @@ app.post("/", (req, res) => {
     .then((url) => {
       if (!url) {
         Url.insertMany({ urlID: shortenId, url: req.body.url }).then(() =>
-          res.render("shorten", { url, shortenId })
+          res.render("shorten", { url, shortenId, image })
         );
       } else {
-        console.log(url);
-        res.render("shorten", { url, shortenId: url.urlID });
+        res.render("shorten", { url, shortenId: url.urlID, image });
       }
     });
 });
